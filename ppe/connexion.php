0a5631836a913674ae0ca40379f654b6fe888fc3
feres92 <117727657+feres92@users.noreply.github.com>
2023-01-16 14:59:12 +0100

    Add files via upload

---
+++ ppe/connexion.php
@@ -0,0 +1,87 @@
+<?php
+session_start();
+ 
+$bdd = new PDO('mysql:host=127.0.0.1;dbname=espace_membre', 'root', '');
+ 
+if(isset($_POST['formconnexion'])) {
+
+   $mailconnect = htmlspecialchars($_POST['mailconnect']);
+   $mdpconnect = sha1($_POST['mdpconnect']);
+
+   if(!empty($mailconnect) AND !empty($mdpconnect)) {
+
+      $requser = $bdd->prepare("SELECT * FROM membre WHERE mail = ? AND mdp = ?");
+      $requser->execute(array($mailconnect, $mdpconnect));
+      $userexist = $requser->rowCount();
+
+      if($userexist == 1) {
+
+         $userinfo = $requser->fetch();       
+         $_SESSION['id'] = $userinfo['ID'];
+         $_SESSION['pseudo'] = $userinfo['Pseudo'];
+         $_SESSION['mail'] = $userinfo['mail'];
+         header("Location: profil.php?id=".$_SESSION['id']);
+
+      } else {
+         $erreur = "Mauvais mail ou mot de passe !";
+
+      }
+   } else {
+      $erreur = "Tous les champs doivent être complétés !";
+   }
+}
+if(isset($_POST['forminscription'])){
+   header("Location: inscription.php?id=".$_SESSION['id']);
+
+} 
+?>
+<html>
+   <head>
+      <title>Connexion client</title>
+      <meta charset="utf-8">
+      <link rel="stylesheet" type="text/css" href="style.css">
+   </head>
+
+   <body>
+         <header class="navbar">
+      <div class="logo">
+         <img src=".\images\allume.png" alt="allumelogo">
+         </div>
+         <nav>
+            <ul>
+
+               <li><a href="index.php">Accueil</a></li>
+               
+               <li><a href="">Produits</a></li>
+               
+               <li><a href="">A propos</a></li>
+               
+               <li><a href="">Contact</a></li>
+               
+               <li><a href="connexion.php">Compte</a></li>
+               
+            </ul>
+            <img src=".\images\panier.png" alt="image2" width="26px">
+         </nav>
+         
+      
+   </header>
+      <div align="center">
+         <h2>Connexion</h2>
+         <br /><br />
+         <form method="POST" action="">
+            <input type="email" name="mailconnect" placeholder="Mail" />
+            <input type="password" name="mdpconnect" placeholder="Mot de passe" />
+            <br /><br />
+            <input type="submit" name="formconnexion" value="Se connecter !" />
+            <input type="submit" name="forminscription" value="S'inscrire !"  />
+            
+         </form>
+         <?php
+         if(isset($erreur)) {
+            echo '<font color="red">'.$erreur."</font>";
+         }
+         ?>
+      </div>
+   </body>
+</html>
---
+++ ppe/index.php
@@ -0,0 +1,159 @@
+<!DOCTYPE html>
+<html>
+<head>
+	<meta charset="utf-8">
+	<meta name="viewport" content="width=device-width, initial-scale=1">
+	<title>ALLU-ME</title>
+	<link rel="stylesheet" type="text/css" href="style.css">
+</head>
+<body>
+	<section class="header-section">
+		
+	
+	<div class="container">
+
+	<header class="navbar">
+	   <div class="logo">
+	   	<img src=".\images\allume.png" alt="allumelogo">
+	   	</div>
+	   	<nav>
+	   		<ul>
+
+	   			<li><a href="">Accueil</a></li>
+	   			
+	   			<li><a href="">Produits</a></li>
+	   			
+	   			<li><a href="">A propos</a></li>
+	   			
+	   			<li><a href="">Contact</a></li>
+	   			
+	   			<li><a href="connexion.php">Compte</a></li>
+	   			
+	   		</ul>
+	   		<img src=".\images\panier.png" alt="image2" width="26px">
+	   	</nav>
+	   	
+	   
+	</header>
+
+	<div class="row"> 
+
+		<div class="col1">
+			<h1>Imaginez vos fenêtres et menuiseries adaptées à votre intérieur !</h1>
+			<p>Choisissez les caractéristiques de vos fenêtres et menuiseries <br>jusqu’à la couleur extérieure de l’aluminium et la finition intérieure du bois.</p>
+			<a href="">Explorer Maintenant</a>
+		</div>
+
+		<div class="col1">
+			<img src=".\images\acc.jpg "alt="image1">
+		</div>
+
+	</div>
+
+
+
+
+
+
+	</div>
+	</section>
+
+
+	<section class="categories">
+		<div class="container">
+			<h2>Top catégories</h2>
+			<div class="row">
+		<div class="col2">
+			<img src=".\images\porte.jpg">
+		</div>
+		<div class="col2">
+			<img src=".\images\vitre.jpg">
+		</div>
+		<div class="col2">
+			<img src=".\images\he.jpg">
+		</div>
+		</div>
+		</div>
+	</section>
+
+	<section class="meilleurs-produits">
+		<div class="container">
+		<h2>Meilleurs produits</h2>
+		<div class="row">
+			<div class="col3">
+				<img src=".\images\porte2.jpg" alt="meilleurprod">
+				<h3>Porte d'entrée aluminium</h3>
+				<p>750 $</p>
+			</div>
+			<div class="col3">
+				<img src="images/baie.jpg" alt="meilleurprod">
+				<h3>Baie coulissante </h3>
+				<p>4500 $</p>
+			</div>
+			<div class="col3">
+				<img src="images/baie2.jpg" alt="meilleurprod">
+				<h3>Porte-fenêtre coulissante </h3>
+				<p>376 $</p>
+			</div>
+			<div class="col3">
+				<img src="images/porte3.jpg" alt="meilleurprod">
+				<h3>Porte coulissante d'intérieur - Bois Blanc</h3>
+				<p>500 $</p>
+			</div>
+		</div>
+		</div>
+	</section>
+
+	<section class="derniers-produits">
+		<div class="container">
+		<h2>Derniers produits</h2>
+		<div class="row">
+			<div class="col3">
+				<img src="images/Volet_aluminium_motorisé.jpg" alt="dernierprod">
+				<h3>Volet en aluminium motorisé</h3>
+				<p>1500 $</p>
+			</div>
+			<div class="col3">
+				<img src="images/fenetre-de-toit_Velux.jpg" alt="dernierprod">
+				<h3>Fenêtre de toit en velux</h3>
+				<p>1000 $</p>
+			</div>
+			<div class="col3">
+				<img src="images/Porte de garage.jpg" alt="dernierprod">
+				<h3>Porte de garage</h3>
+				<p>930$</p>
+			</div>
+			<div class="col3">
+				<img src="images/dproduit4.jpg" alt="dernierprod">
+				<h3>(Nom produit)</h3>
+				<p>5000 $</p>
+			</div>
+		</div>
+
+		<div class="row">
+			<div class="col3">
+				<img src="images/dproduit1.jpg" alt="dernierprod">
+				<h3>(Nom produit)</h3>
+				<p>5000 $</p>
+			</div>
+			<div class="col3">
+				<img src="images/dproduit2.jpg" alt="dernierprod">
+				<h3>(Nom produit)</h3>
+				<p>5000 $</p>
+			</div>
+			<div class="col3">
+				<img src="images/dproduit3.jpg" alt="dernierprod">
+				<h3>(Nom produit)</h3>
+				<p>5000 $</p>
+			</div>
+			<div class="col3">
+				<img src="images/dproduit4.jpg" alt="dernierprod">
+				<h3>(Nom produit)</h3>
+				<p>5000 $</p>
+			</div>
+		</div>
+		</div>
+	</section>
+
+</body>
+</html>
